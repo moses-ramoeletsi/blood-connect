3559d5ab89ccbd89e7155ded041bc8b41104ed58
ahhhhmoses <140169181+moses-ramoeletsi@users.noreply.github.com>
2024-04-08 15:14:12 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bloodConnect',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
