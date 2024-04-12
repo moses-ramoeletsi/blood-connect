18f4fc52daa7ba63f220d148a3b74a4cd668c675
ahhhhmoses <140169181+moses-ramoeletsi@users.noreply.github.com>
2024-04-12 15:14:52 +0200

    added user details file

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/compat/auth';
+import {AngularFirestore} from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    public firebaseStore: AngularFirestore,
+    public userAuth: AngularFireAuth
+  ) { }
+
+  signupWithEmail(data: {email: string; password: string;}) {
+    return this.userAuth.createUserWithEmailAndPassword(data.email, data.password)
+  }
+  saveUserDetails(data: any){
+    return this.firebaseStore.collection("users").doc(data.uid).set(data);
+  }
 }
