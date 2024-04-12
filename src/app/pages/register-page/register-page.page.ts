689e92897c386c34e20da2cb420a24d9d9702975
ahhhhmoses <140169181+moses-ramoeletsi@users.noreply.github.com>
2024-04-12 15:16:43 +0200

    added register auth funtion

--- src/app/pages/register-page/register-page.page.ts
+++ src/app/pages/register-page/register-page.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-register-page',
@@ -6,10 +9,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-page.page.scss'],
 })
 export class RegisterPagePage implements OnInit {
+userData ={
+  firstName: "",
+  lastName: "",
+  email: "",
+  address: "",
+  phoneNumber: "",
+  password: "",
+  uid: ""
+};
 
-  constructor() { }
+  constructor(
+    public fireserviceStore: UserService,
+    public alertController: AlertController,
+    public router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  signup() {
+    this.fireserviceStore
+      .signupWithEmail(this.userData)
+      .then((userDettails) => {
+        const user = userDettails.user;
+        this.userData.uid = user?.uid as string;
+        this.fireserviceStore
+          .saveUserDetails(this.userData)
+          .then(() => {
+            this.showAlert(
+              'Registration Successful',
+              'You are now registered!'
+            );
+            this.router.navigate(['/login-page']);
+          })
+          .catch((error) => {
+            console.error('Error saving user details', error);
+          });
+      })
+      .catch((error)=>{
+        this.showAlert(
+          'Registration Error',
+          'An error occurred during registration.'
+        );
+      });
+  }
+
+  showAlert(title: string, message: string) {
+    this.alertController
+      .create({
+        header: title,
+        message: message,
+        buttons: ['OK'],
+      })
+      .then((alert) => alert.present());
   }
 
 }
