0987d15c29cd8aa609fd2ba4bd448696c7371a18
ahhhhmoses <140169181+moses-ramoeletsi@users.noreply.github.com>
2024-04-13 02:42:30 +0200

    added user login function

--- src/app/pages/login-page/login-page.page.ts
+++ src/app/pages/login-page/login-page.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPagePage implements OnInit {
 
-  constructor() { }
+  userLogin = {
+    email: "",
+    password: ""
+  }
+
+  constructor(
+    public fireServices: UserService,
+    public alertController: AlertController,
+    public router: Router,
+
+  ) { }
 
   ngOnInit() {
   }
 
+  login() {
+    this.fireServices
+      .loginWithEmail(this.userLogin)
+      .then((userDetails) => {
+        const user = userDetails.user;
+
+        this.fireServices.getUserDetails(user).subscribe((userData) => {
+          if (userData) {
+            this.router.navigate(['/home']);
+          } else {
+            this.showAlert(
+              'Login Failed',
+              'An error occurred while trying to log in.'
+            );
+          }
+        });
+      })
+      .catch((error) => {
+        console.error('Error logging in:', error);
+        this.showAlert('User Not Found', 'The user does not exist.');
+      });
+  }
+  
+  showAlert(title: string, message: string) {
+    this.alertController
+      .create({
+        header: title,
+        message: message,
+        buttons: ['OK'],
+      })
+      .then((alert) => alert.present());
+  }
 }
+
+
