2e86a2c106f47bf89a4ae8d1f4a21dae3e725aa3
ahhhhmoses <140169181+moses-ramoeletsi@users.noreply.github.com>
2024-04-13 04:06:46 +0200

    added blood request details

---
+++ src/app/shared/requestBloodDetails.ts
@@ -0,0 +1,8 @@
+export interface BloodRequestDetails{
+    firstName: string;
+    address: string;
+    phoneNumber: number;
+    bloodGroup: string;
+    transfusionType: string;
+    location:string;
+}
