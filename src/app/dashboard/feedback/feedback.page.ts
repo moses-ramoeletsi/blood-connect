aae4bcf76c4caa6bd1978962bcd064a4ef56374d
ahhhhmoses <140169181+moses-ramoeletsi@users.noreply.github.com>
2024-04-14 08:50:58 +0200

    inserted user feedback into database

--- src/app/dashboard/feedback/feedback.page.ts
+++ src/app/dashboard/feedback/feedback.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { UserFeedBackService } from 'src/app/services/user-feed-back.service';
 
 @Component({
   selector: 'app-feedback',
@@ -7,8 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackPage implements OnInit {
 
-  constructor() { }
+  feedBackForm = {
+    name:"",
+    email:"",
+    feedBackType: "",
+    message:""
+  }
+  constructor(public fireServices:UserFeedBackService, public alertController: AlertController) { }
 
+  submitFeedBack() {
+    this.fireServices
+      .userfeedBack(this.feedBackForm)
+      .then((res) => {
+        this.showAlert('FeedBack', 'FeedBack sent successfully!');
+      })
+      .catch((error) => {
+        this.showAlert(
+          'FeedBack',
+          'Error occured! \n FeedBack not successful'
+        );
+        console.error('Error adding request: ', error);
+      });
+  }
+  showAlert(title: string, message: string) {
+    this.alertController
+      .create({
+        header: title,
+        message: message,
+        buttons: ['OK'],
+      })
+      .then((alert) => alert.present());
+  }
   ngOnInit() {
   }
 
