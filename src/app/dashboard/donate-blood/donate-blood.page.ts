a6fa44f8171948e880f29305e8f1a41e6b446cfe
ahhhhmoses <140169181+moses-ramoeletsi@users.noreply.github.com>
2024-04-15 12:31:59 +0200

    added geolocation

--- src/app/dashboard/donate-blood/donate-blood.page.ts
+++ src/app/dashboard/donate-blood/donate-blood.page.ts
@@ -3,6 +3,16 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AlertController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { DonateBloodService } from 'src/app/services/donate-blood.service';
+import { Geolocation } from '@capacitor/geolocation';
+import {
+  LatLng,
+  LatLngTuple,
+  LeafletMouseEvent,
+  Map,
+  icon,
+  marker,
+  tileLayer,
+} from 'leaflet';
 
 @Component({
   selector: 'app-donate-blood',
@@ -11,12 +21,17 @@ import { DonateBloodService } from 'src/app/services/donate-blood.service';
 })
 export class DonateBloodPage implements OnInit {
   recipients!: Observable<any[]>;
+  map: any;
+  marker: any;
+  mapInitialized: boolean = false;
+  userMarker: any;
   bloodDonorForm = {
     firstName: '',
     address: '',
     phoneNumber: '',
     bloodGroup: '',
     transfusionType: '',
+    location: '',
   };
   bloodGroups: string[] = ['A+', 'B+', 'AB+', 'O+', 'A-', 'B-', 'AB-', 'O-'];
   showNearBySeekerContent: boolean = false;
@@ -33,13 +48,81 @@ export class DonateBloodPage implements OnInit {
     this.fetchRecipients();
   }
 
+  ngAfterViewInit() {
+    this.getLocation();
+  }
+  initializeMap(center: LatLngTuple) {
+    if (!this.mapInitialized) {
+      this.map = new Map('map', {
+        center: center,
+        zoom: 8,
+      });
+
+      const tiles = tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+          maxZoom: 20,
+          minZoom: 4,
+        }
+      );
+
+      tiles.addTo(this.map);
+
+      this.map.on('click', (event: LeafletMouseEvent) => {
+        this.updateMarkerPosition(event.latlng);
+      });
+
+      this.mapInitialized = true;
+    }
+  }
+  getLocation() {
+    Geolocation['getCurrentPosition']()
+      .then((position: any) => {
+        const geoPosition: GeolocationPosition =
+          position as GeolocationPosition;
+        const { latitude, longitude } = geoPosition.coords;
+
+        const userLocation: LatLngTuple = [latitude, longitude];
+
+        this.initializeMap(userLocation);
+
+        this.userMarker = marker(userLocation).addTo(this.map);
+
+        const customIcon = icon({
+          iconUrl: 'assets/images/pin.png',
+          iconSize: [25, 41],
+          iconAnchor: [12, 41],
+          popupAnchor: [1, -34],
+        });
+
+        this.userMarker.setIcon(customIcon);
+
+        this.updateCoordinatesInput(userLocation);
+      })
+      .catch((error) => {
+        console.error('Error getting user location:', error);
+      });
+  }
+  updateMarkerPosition(position: LatLng) {
+    if (this.userMarker) {
+      const { lat, lng } = position;
+      const newPosition: LatLngTuple = [lat, lng];
+      this.userMarker.setLatLng(newPosition);
+      this.updateCoordinatesInput(newPosition);
+    }
+  }
+  updateCoordinatesInput(position: LatLngTuple) {
+    this.bloodDonorForm.location = `${position[0]}, ${position[1]}`;
+  }
   fetchRecipients() {
     if (this.pickedBloodGroup) {
-      this.recipients = this.fireStore.collection('requests', ref =>
-        ref.where('bloodGroup', '==', this.pickedBloodGroup)
-      ).valueChanges();
+      this.recipients = this.fireStore
+        .collection('recipiecnts', (ref) =>
+          ref.where('bloodGroup', '==', this.pickedBloodGroup)
+        )
+        .valueChanges();
     } else {
-      this.recipients = this.fireStore.collection('requests').valueChanges();
+      this.recipients = this.fireStore.collection('recipiecnts').valueChanges();
     }
   }
 
@@ -61,7 +144,7 @@ export class DonateBloodPage implements OnInit {
     this.firebaseService
       .donateBlood(this.bloodDonorForm)
       .then((res) => {
-        this.showAlert('Donor Posted', 'Donor added successfully!');
+        this.showAlert('Donor Posted', 'Donor successfully added!');
       })
       .catch((error) => {
         this.showAlert(
