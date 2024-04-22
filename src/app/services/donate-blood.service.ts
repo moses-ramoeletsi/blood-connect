9ca19a89d6a79d986ba6e064986d7050fb5ce1e9
ahhhhmoses <140169181+moses-ramoeletsi@users.noreply.github.com>
2024-04-22 13:32:03 +0200

    modified donate blood service

--- src/app/services/donate-blood.service.ts
+++ src/app/services/donate-blood.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { BloodDonorDetails } from '../shared/bloodDonorDetails';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
