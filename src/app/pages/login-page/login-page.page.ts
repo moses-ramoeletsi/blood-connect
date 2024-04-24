31099e801f58f580ab7361f5d751ecc897544e8f
ahhhhmoses <140169181+moses-ramoeletsi@users.noreply.github.com>
2024-04-24 17:49:42 +0200

    added reactive form

--- src/app/pages/login-page/login-page.page.ts
+++ src/app/pages/login-page/login-page.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 import { AlertController } from '@ionic/angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -9,6 +10,7 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./login-page.page.scss'],
 })
 export class LoginPagePage implements OnInit {
+  loginForm!: FormGroup;
   userLogin = {
     email: '',
     password: '',
@@ -20,28 +22,42 @@ export class LoginPagePage implements OnInit {
     public router: Router
   ) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(/[a-zA-Z0-9@_\-!]+/),
+      ]),
+    });
+  }
 
   login() {
-    this.fireServices
-      .loginWithEmail(this.userLogin)
-      .then((userDetails) => {
-        const user = userDetails.user;
-
-        this.fireServices.getUserDetails(user).subscribe((userData) => {
-          if (userData) {
-            this.router.navigate(['/home']);
-          } else {
-            this.showAlert(
-              'Login Failed',
-              'An error occurred while trying to log in.'
-            );
-          }
+    if(this.loginForm.valid){
+      const userLogin = this.loginForm.value;
+      this.fireServices
+        .loginWithEmail(userLogin)
+        .then((userDetails) => {
+          const user = userDetails.user;
+  
+          this.fireServices.getUserDetails(user).subscribe((userData) => {
+            if (userData) {
+              this.router.navigate(['/home']);
+            } else {
+              this.showAlert(
+                'Login Failed',
+                'An error occurred while trying to log in.'
+              );
+            }
+          });
+        })
+        .catch((error) => {
+          this.showAlert('User Not Found', 'The user does not exist.');
         });
-      })
-      .catch((error) => {
-        this.showAlert('User Not Found', 'The user does not exist.');
-      });
+    }else {
+      this.showAlert('Form Error', 'Please check the form fields.');
+    }
   }
 
   showAlert(title: string, message: string) {
