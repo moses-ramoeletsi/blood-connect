e5bd3e5b67a4ccee8269f4caf0a03e4b0c988e30
ahhhhmoses <140169181+moses-ramoeletsi@users.noreply.github.com>
2024-04-24 17:24:29 +0200

    added reavtive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,22 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireStorageModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide:RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/register-page/register-page.page.ts
+++ src/app/pages/register-page/register-page.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -9,6 +10,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./register-page.page.scss'],
 })
 export class RegisterPagePage implements OnInit {
+  registerForm!: FormGroup;
   userData = {
     firstName: '',
     lastName: '',
@@ -25,27 +27,50 @@ export class RegisterPagePage implements OnInit {
     public router: Router
   ) {}
 
-  ngOnInit() {}
-
-  signup() {
-    this.fireserviceStore
-      .signupWithEmail(this.userData)
-      .then((userDettails) => {
-        const user = userDettails.user;
-        this.userData.uid = user?.uid as string;
-        this.fireserviceStore.saveUserDetails(this.userData).then(() => {
-          this.showAlert('Registration Successful', 'You are now registered!');
-          this.router.navigate(['/login-page']);
-        });
-      })
-      .catch((error) => {
-        this.showAlert(
-          'Registration Error',
-          'An error occurred during registration.'
-        );
-      });
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      address: new FormControl('', Validators.required),
+      phoneNumber: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[5-6]\d{7,}$/),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(/[a-zA-Z0-9@_\-!]+/),
+      ]),
+    });
   }
 
+  signup() {
+    if (this.registerForm.valid) {
+      const userData = this.registerForm.value;
+      this.fireserviceStore
+        .signupWithEmail(userData)
+        .then((userDetails) => {
+          const user = userDetails.user;
+          userData.uid = user?.uid as string;
+          this.fireserviceStore.saveUserDetails(userData).then(() => {
+            this.showAlert(
+              'Registration Successful',
+              'You are now registered!'
+            );
+            this.router.navigate(['/login-page']);
+          });
+        })
+        .catch((error) => {
+          this.showAlert(
+            'Registration Error',
+            'An error occurred during registration.'
+          );
+        });
+    } else {
+      this.showAlert('Form Error', 'Please check the form fields.');
+    }
+  }
   showAlert(title: string, message: string) {
     this.alertController
       .create({
