3d83a62c63d6b65ce972f3cb27e7acf614121293
ahhhhmoses <140169181+moses-ramoeletsi@users.noreply.github.com>
2024-04-25 08:21:39 +0200

    modified

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBCeeb0q-hAs6OHFYAuXW5awqee63cWPv0",
+    authDomain: "bloodconnect-c897c.firebaseapp.com",
+    projectId: "bloodconnect-c897c",
+    storageBucket: "bloodconnect-c897c.appspot.com",
+    messagingSenderId: "46386487924",
+    appId: "1:46386487924:web:5059a0f015c9b8ccc12721"
+  }
 };
